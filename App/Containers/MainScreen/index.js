0f242e819d6e7689ce5b4d565b49df9ca7d5eb3d
HONEYNET <admin@Admins-iMac.local>
2018-10-10 17:50:51 +0700

    <>

--- App/Containers/MainScreen/index.js
+++ App/Containers/MainScreen/index.js
@@ -2,46 +2,68 @@ import React from 'react'
 import { Text, View, FlatList, TouchableOpacity } from 'react-native'
 import { create } from 'apisauce'
 import FastImage from 'react-native-fast-image'
+import Config from '../../Config'
 import { Metrics } from '../../Theme'
 
 const LLSIFApiClient = create({
-  baseURL: Config.API_URL,
+  baseURL: 'https://schoolido.lu/api/',//Config.API_URL,
   timeout: 3000,
 })
 
 export default class MainScreen extends React.Component {
-  state = {
-    isLoading: true,
-    data: null,
-    page: 1
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+      data: [],
+      page: 1
+    }
   }
 
   async getCards(page) {
-    await data = LLSIFApiClient.get(Config.CARDS, { ordering: '-release_date', page: page })
-    this.setState({ data: data, isLoading: true, page: this.state.page + 1 })
+    var data = await LLSIFApiClient.get('cards', { ordering: '-release_date', page: page })
+    var arrayData = data.data.results.map((item) => {
+      return {
+        id: item.id,
+        rarity: item.rarity,
+        attribute: item.attribute,
+        cardImage: item.card_image ? 'https:' + item.card_image : null,
+        cardIdolizedImage: item.card_idolized_image ? 'https:' + item.card_idolized_image : null,
+        idol: {
+          name: item.idol.name,
+          school: item.idol.school,
+          year: item.idol.year,
+          mainUnit: item.idol.main_unit,
+          subUnit: item.idol.sub_unit
+        }
+      }
+    })
+    var x = [...this.state.data, ...arrayData]
+    this.setState({ data: x, isLoading: false, page: this.state.page + 1 })
   }
 
   _keyExtractor = (item, index) => 'card' + item.id;
 
   _renderItem = ({ item }) => (
-    <TouchableOpacity style={{ width: Metrics.fullWidth / 2 }}>
+    <TouchableOpacity style={{ width: Metrics.fullWidth / 2, backgroundColor: 'white', }}>
       <FastImage
+        style={{ width: Metrics.fullWidth / 2, height: 200 }}
         source={{
-          uri: item.card_image,
+          uri: item.cardImage ? item.cardImage : item.cardIdolizedImage,
           priority: FastImage.priority.normal,
         }} />
       <Text>Name: {item.idol.name}</Text>
       <Text>School: {item.idol.school}</Text>
       <Text>Year: {item.idol.year}</Text>
-      <Text>Main unit: {item.idol.main_unit}</Text>
+      <Text>Main unit: {item.idol.mainUnit}</Text>
       <Text>Sub unit: {item.idol.sub_unit}</Text>
-      <Text>Rarity: {item.idol.rarity}</Text>
-      <Text>Attribute: {item.idol.attribute}</Text>
+      <Text>Rarity: {item.rarity}</Text>
+      <Text>Attribute: {item.attribute}</Text>
     </TouchableOpacity>
   );
 
   componentDidMount() {
-    getCards(1)
+    this.getCards(1)
   }
 
   render() {

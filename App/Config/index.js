a298a604a0b3d5e26ae70e629cfb85db94c87fb5
HONEYNET <thachxyz123@gmail.com>
2018-11-23 14:53:55 +0700

    Update log

--- App/Config/index.js
+++ App/Config/index.js
@@ -11,5 +11,5 @@ export const Config = {
   CARDIDS: 'cardids/',
   GITHUB_PROJECT: 'https://github.com/pnthach95/LLSIFTomodachiApp',
   DEFAULT_BACKGROUND: '//i.schoolido.lu/c/1722idolizedRuby.png',
-  DRAWER: `This app is open-source.\n\nPress Version to view background.\n\nChange log:\n- Add drawer button.\n- Add "Worldwide only" option.\n- Fix chibi images in Idol tab.\n- Fix 404 error.\n- Change some UIs.`
+  DRAWER: `This app is open-source.\n\nPress Version to view background.\n\nChange log:\n- Add drawer button.\n- Add "Worldwide only" option.\n- Add card viewer.\n- Fix chibi images in Idol tab.\n- Fix 404 error.\n- Change some UIs.`
 }

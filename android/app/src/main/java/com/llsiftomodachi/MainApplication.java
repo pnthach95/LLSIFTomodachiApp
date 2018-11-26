0036fbb24262c9534d3fd476ba1cc79696318fc6
Pham Ngoc Thach <thachxyz123@gmail.com>
2018-11-26 10:35:50 +0700

    Add sentry

--- android/app/src/main/java/com/llsiftomodachi/MainApplication.java
+++ android/app/src/main/java/com/llsiftomodachi/MainApplication.java
@@ -3,6 +3,7 @@ package com.llsiftomodachi;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.sentry.RNSentryPackage;
 
 import com.apsl.versionnumber.RNVersionNumberPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSentryPackage(),
             new RNVersionNumberPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage(),

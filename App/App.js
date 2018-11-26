1f3fe3415c36716dd786df60c0fc1afdccf15762
Pham Ngoc Thach <thachxyz123@gmail.com>
2018-11-26 22:41:21 +0700

    Update sentry

--- App/App.js
+++ App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/lib/integration/react'
+import { Sentry } from 'react-native-sentry'
 import createStore from './Stores'
 import RootScreen from './Containers/Root/RootScreen'
 import SplashScreen from './Containers/SplashScreen/SplashScreen'
 import { Colors } from './Theme'
 
+Sentry.config('https://ac9aa894ab9341fba115b29731378b6b@sentry.io/1330276').install()
 const { store, persistor } = createStore()
 
 export default class App extends Component {

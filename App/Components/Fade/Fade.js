feb32e400ffe5e76ce90d97103d8e55c6a41dfdf
Phạm Ngọc Thạch <thachxyz123@gmail.com>
2018-12-17 16:16:21 +0700

    Fix animation

--- App/Components/Fade/Fade.js
+++ App/Components/Fade/Fade.js
@@ -27,7 +27,7 @@ export default class Fade extends Component {
     }
     Animated.timing(this._visibility, {
       toValue: nextProps.visible ? 1 : 0,
-      duration: 3000
+      duration: 500
     }).start(() => {
       this.setState({ visible: nextProps.visible });
     });

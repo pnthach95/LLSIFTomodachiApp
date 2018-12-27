7a9725517afca381587ec81a07084aa7c993ceb6
Phạm Ngọc Thạch <thachxyz123@gmail.com>
2018-12-27 09:01:35 +0700

    Add Victory Native

--- App/Containers/EventDetailScreen/Tracker.js
+++ App/Containers/EventDetailScreen/Tracker.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, ScrollView, Text, FlatList } from 'react-native';
+import { View, ScrollView, Text, FlatList } from 'react-native';
 import SegmentedControlTab from 'react-native-segmented-control-tab';
 import { Table, Row } from 'react-native-table-component';
+import { VictoryBar, VictoryChart, VictoryTheme } from 'victory-native';
 
 import { ApplicationStyles } from '../../Theme';
 import styles from './styles';
@@ -10,7 +11,7 @@ export default class Tracker extends React.PureComponent {
 	constructor(props) {
 		super(props);
 		this.state = {
-			tableHead: ['date', 't1', 't1_diff', 't2', 't2_diff', 't3', 't3_diff', 't1_song', 't1_song_diff', 't2_song', 't2_song_diff', 't3_song', 't3_song_diff'],
+			tableHead: ['Date', 't1', 't1_diff', 't2', 't2_diff', 't3', 't3_diff', 't1_song', 't1_song_diff', 't2_song', 't2_song_diff', 't3_song', 't3_song_diff'],
 			widCell: [120, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80, 80],
 			selectedTab: this.props.wwTracker === null ? 1 : 0
 		}
@@ -24,6 +25,23 @@ export default class Tracker extends React.PureComponent {
 		widthArr={this.state.widCell}
 		textStyle={styles.trackerText} />
 
+	_renderTable = (data) =>
+		<ScrollView horizontal={data !== null}>
+			{data !== null
+				? <View>
+					<Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
+						<Row data={this.state.tableHead}
+							widthArr={this.state.widCell}
+							style={styles.trackerHead}
+							textStyle={styles.trackerText} />
+					</Table>
+					<FlatList data={data}
+						keyExtractor={this._keyExtractor}
+						renderItem={this._renderItem} />
+				</View>
+				: <Text style={styles.whiteCenter}>No data</Text>}
+		</ScrollView>
+
 	render() {
 		return (
 			<View style={styles.container}>
@@ -32,37 +50,24 @@ export default class Tracker extends React.PureComponent {
 						selectedIndex={this.state.selectedTab}
 						onTabPress={this._onTabPress} />
 				</View>
+				<VictoryChart theme={VictoryTheme.material}>
+					<VictoryLine
+						style={{
+							data: { stroke: "#c43a31" },
+							parent: { border: "1px solid #ccc" }
+						}}
+						data={[
+							{ x: 1, y: 2 },
+							{ x: 2, y: 3 },
+							{ x: 3, y: 5 },
+							{ x: 4, y: 4 },
+							{ x: 5, y: 7 }
+						]}
+					/>
+				</VictoryChart>
 				{this.state.selectedTab === 0
-					? <ScrollView horizontal={this.props.wwTracker !== null}>
-						{this.props.wwTracker === null
-							? <Text style={styles.whiteCenter}>No data</Text>
-							: <View>
-								<Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
-									<Row data={this.state.tableHead}
-										widthArr={this.state.widCell}
-										style={styles.trackerHead}
-										textStyle={styles.trackerText} />
-								</Table>
-								<FlatList data={this.props.wwTracker}
-									keyExtractor={this._keyExtractor}
-									renderItem={this._renderItem} />
-							</View>}
-					</ScrollView>
-					: <ScrollView horizontal={this.props.jpTracker !== null}>
-						{this.props.jpTracker === null
-							? <Text style={styles.whiteCenter}>No data</Text>
-							: <View>
-								<Table borderStyle={{ borderWidth: 2, borderColor: '#c8e1ff' }}>
-									<Row data={this.state.tableHead}
-										widthArr={this.state.widCell}
-										style={styles.trackerHead}
-										textStyle={styles.trackerText} />
-								</Table>
-								<FlatList data={this.props.jpTracker}
-									keyExtractor={this._keyExtractor}
-									renderItem={this._renderItem} />
-							</View>}
-					</ScrollView>}
+					? this._renderTable(this.props.wwTracker)
+					: this._renderTable(this.props.jpTracker)}
 			</View>
 		)
 	}
--- android/app/src/main/java/com/pnthach95/llsiftomodachi/MainApplication.java
+++ android/app/src/main/java/com/pnthach95/llsiftomodachi/MainApplication.java
@@ -3,6 +3,7 @@ package com.pnthach95.llsiftomodachi;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
 import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
           new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
           new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
           new AppCenterReactNativePackage(MainApplication.this),

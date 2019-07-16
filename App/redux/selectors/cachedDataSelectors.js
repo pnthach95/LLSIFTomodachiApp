31eedbe80cdd8a58c6af08489abaeed7bda0eb6a
Pham Ngoc Thach <thachxyz123@gmail.com>
2019-07-16 13:51:23 +0700

    Remove unused function

--- App/redux/selectors/cachedDataSelectors.js
+++ App/redux/selectors/cachedDataSelectors.js
@@ -1,9 +1,5 @@
-export const getCachedData = state => state.cachedData;
-
-export const getMaxStats = state => state.cachedData.cards_info.max_stats;
-
 export const getIdols = (state) => {
-  const x = state.cachedData.cards_info.idols;
+  const x = state.reducer.cachedData.cards_info.idols;
   const y = x.map((item) => {
     const { name } = item;
     return { label: name, value: name };
@@ -12,57 +8,22 @@ export const getIdols = (state) => {
 };
 
 export const getSubunits = (state) => {
-  const y = state.cachedData.cards_info.sub_units;
+  const y = state.reducer.cachedData.cards_info.sub_units;
   const z = y.map(item => ({ label: item, value: item }));
   return z;
 };
 
 export const getSchools = (state) => {
-  const y = state.cachedData.cards_info.schools;
+  const y = state.reducer.cachedData.cards_info.schools;
   const z = y.map(item => ({ label: item, value: item }));
   return z;
 };
 
 export const getSkills = (state) => {
-  const x = state.cachedData.cards_info.skills;
+  const x = state.reducer.cachedData.cards_info.skills;
   const y = x.map((item) => {
     const { skill } = item;
     return { label: skill, value: skill };
   });
   return y;
-};
-
-export const getSongMaxStat = state => state.cachedData.cards_info.songs_max_stats;
-
-export const getRandomCard = (state) => {
-  try {
-    const x = state.cachedData.randomCard.idol;
-    return x;
-  } catch (error) {
-    return null;
-  }
-};
-
-export const getBGImage = (state) => {
-  try {
-    return state.cachedData.bgImage;
-  } catch (error) {
-    return null;
-  }
-};
-
-export const getWWEventInfo = (state) => {
-  try {
-    return state.cachedData.eventInfo.ww;
-  } catch (error) {
-    return null;
-  }
-};
-
-export const getJPEventInfo = (state) => {
-  try {
-    return state.cachedData.eventInfo.jp;
-  } catch (error) {
-    return null;
-  }
 };

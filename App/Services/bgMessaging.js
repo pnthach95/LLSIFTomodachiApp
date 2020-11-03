652f304c8bfdda70ef2ea7b6ace63f9ea2d2fc68
Pham Ngoc Thach <thachxyz123@gmail.com>
2020-11-03 16:02:54 +0700

    Update

--- App/Services/bgMessaging.js
+++ App/Services/bgMessaging.js
@@ -1,5 +1,6 @@
-/* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import firebase from 'react-native-firebase';
 
 export default async (message) =>

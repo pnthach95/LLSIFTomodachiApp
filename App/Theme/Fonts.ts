537c20698a3ce8c4bbebe428edf76a4af509817e
Pham Ngoc Thach <thachxyz123@gmail.com>
2020-11-12 13:11:58 +0700

    Remove

--- App/Theme/Fonts.ts
+++ App/Theme/Fonts.ts
@@ -1,34 +1,12 @@
 import { StyleSheet } from 'react-native';
 import { Colors } from 'react-native-paper';
 
-const size = {
-  h1: 38,
-  h2: 34,
-  h3: 30,
-  input: 18,
-  regular: 17,
-  medium: 14,
-  small: 12
-};
-
 const style = StyleSheet.create({
   black: {
     color: Colors.black
   },
   center: {
     textAlign: 'center'
-  },
-  h1: {
-    fontSize: size.h1
-  },
-  h2: {
-    fontSize: size.h2
-  },
-  h3: {
-    fontSize: size.h3
-  },
-  normal: {
-    fontSize: size.regular
   },
   textWrap: {
     flexGrow: 1,
@@ -40,4 +18,4 @@ const style = StyleSheet.create({
   }
 });
 
-export default { size, style };
+export default { style };

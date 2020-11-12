d8babe13cdd08b9dcbc5b2e7d80e07fbe26f8f8c
Pham Ngoc Thach <thachxyz123@gmail.com>
2020-11-12 11:04:05 +0700

    Reverse

--- App/routes/routes.tsx
+++ App/routes/routes.tsx
@@ -1,11 +1,10 @@
 import React, { useContext } from 'react';
-import { View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { ModalProvider } from 'react-native-modalfy';
-import { Provider as PaperProvider, useTheme } from 'react-native-paper';
+import { Provider as PaperProvider } from 'react-native-paper';
 
-import { AppStyles, Dark, Light } from '~/Theme';
+import { Dark, Light } from '~/Theme';
 import UserContext from '~/Context/UserContext';
 import Modals from '~/modals';
 
@@ -26,90 +25,83 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 const Routes = (): JSX.Element => {
   const { state } = useContext(UserContext);
-  const { colors } = useTheme();
-  const bgColor = { backgroundColor: colors.background };
 
   return (
     <PaperProvider theme={state.options.isDark ? Dark : Light}>
       <ModalProvider stack={Modals}>
-        <View style={[AppStyles.screen, bgColor]}>
-          <NavigationContainer theme={state.options.isDark ? Dark : Light}>
-            <Stack.Navigator>
-              {state.loading ? (
+        <NavigationContainer theme={state.options.isDark ? Dark : Light}>
+          <Stack.Navigator>
+            {state.loading ? (
+              <Stack.Screen
+                name='SplashScreen'
+                component={SplashScreen}
+                options={{
+                  headerShown: false
+                }}
+              />
+            ) : (
+              <>
                 <Stack.Screen
-                  name='SplashScreen'
-                  component={SplashScreen}
+                  name='Main'
+                  component={LLSIFTab}
                   options={{
                     headerShown: false
                   }}
                 />
-              ) : (
-                <>
-                  <Stack.Screen
-                    name='Main'
-                    component={LLSIFTab}
-                    options={{
-                      headerShown: false
-                    }}
-                  />
-                  <Stack.Screen
-                    name='IdolsScreen'
-                    component={IdolsScreen}
-                    options={{
-                      title: 'School Idols'
-                    }}
-                  />
-                  <Stack.Screen
-                    name='SongsScreen'
-                    component={SongsScreen}
-                    options={{
-                      headerShown: false
-                    }}
-                  />
-                  <Stack.Screen
-                    name='CardDetailScreen'
-                    component={CardDetailScreen}
-                    options={{
-                      headerShown: false
-                    }}
-                  />
-                  <Stack.Screen
-                    name='EventDetailScreen'
-                    component={EventDetailScreen}
-                    options={{
-                      title: ''
-                    }}
-                  />
-                  <Stack.Screen
-                    name='EventTrackerScreen'
-                    component={EventTrackerScreen}
-                    options={{
-                      title: ''
-                    }}
-                  />
-                  <Stack.Screen
-                    name='IdolDetailScreen'
-                    component={IdolDetailScreen}
-                    options={{
-                      headerShown: false
-                    }}
-                  />
-                  <Stack.Screen
-                    name='SongDetailScreen'
-                    component={SongDetailScreen}
-                    options={{
-                      headerShown: false
-                    }}
-                  />
-                  <Stack.Screen
-                    name='AboutMeScreen'
-                    component={AboutMeScreen}
-                  />
-                </>
-              )}
-            </Stack.Navigator>
-          </NavigationContainer>
-        </View>
+                <Stack.Screen
+                  name='IdolsScreen'
+                  component={IdolsScreen}
+                  options={{
+                    title: 'School Idols'
+                  }}
+                />
+                <Stack.Screen
+                  name='SongsScreen'
+                  component={SongsScreen}
+                  options={{
+                    headerShown: false
+                  }}
+                />
+                <Stack.Screen
+                  name='CardDetailScreen'
+                  component={CardDetailScreen}
+                  options={{
+                    headerShown: false
+                  }}
+                />
+                <Stack.Screen
+                  name='EventDetailScreen'
+                  component={EventDetailScreen}
+                  options={{
+                    title: ''
+                  }}
+                />
+                <Stack.Screen
+                  name='EventTrackerScreen'
+                  component={EventTrackerScreen}
+                  options={{
+                    title: ''
+                  }}
+                />
+                <Stack.Screen
+                  name='IdolDetailScreen'
+                  component={IdolDetailScreen}
+                  options={{
+                    headerShown: false
+                  }}
+                />
+                <Stack.Screen
+                  name='SongDetailScreen'
+                  component={SongDetailScreen}
+                  options={{
+                    headerShown: false
+                  }}
+                />
+                <Stack.Screen name='AboutMeScreen' component={AboutMeScreen} />
+              </>
+            )}
+          </Stack.Navigator>
+        </NavigationContainer>
       </ModalProvider>
     </PaperProvider>
   );

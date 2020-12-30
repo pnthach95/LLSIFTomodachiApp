273d3406325a53311bdcf58916263e5fb48ef681
Pham Ngoc Thach <thachxyz123@gmail.com>
2020-12-30 14:43:20 +0700

    Format

--- babel.config.js
+++ babel.config.js
@@ -1,21 +1,12 @@
-const plugins = [];
-
-// if (process.env.NODE_ENV !== 'production') {
-//   plugins.push('babel-plugin-typescript-to-proptypes');
-// }
-
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
-    ...plugins,
     [
       'module-resolver',
       {
-        root: [
-          './App',
-        ],
+        root: ['./App'],
         alias: {
-          "^~/(.+)": "./App/\\1"
+          '^~/(.+)': './App/\\1',
         },
       },
     ],

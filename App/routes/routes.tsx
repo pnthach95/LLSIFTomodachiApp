01d779af6517c41c93423bef9180a04f05d9304b
Pham Ngoc Thach <thachxyz123@gmail.com>
2020-12-31 10:03:20 +0700

    noHeader

--- App/routes/routes.tsx
+++ App/routes/routes.tsx
@@ -64,7 +64,7 @@ const Routes = (): JSX.Element => {
                 <Stack.Screen
                   name='EventDetailScreen'
                   component={EventDetailScreen}
-                  options={{ headerShown: false }}
+                  options={noHeader}
                 />
                 <Stack.Screen
                   name='EventTrackerScreen'

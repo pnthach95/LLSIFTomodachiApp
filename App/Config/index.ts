01487234e8fb7b61f29c5284921210431ebeac17
Pham Ngoc Thach <thachxyz123@gmail.com>
2020-12-31 14:48:03 +0700

    Update change log

--- App/Config/index.ts
+++ App/Config/index.ts
@@ -51,6 +51,9 @@ export const OrderingGroup = {
 export const RELEASE_NOTE = `This app is free and open-source.
 
 Change log:
+- Version 0.4.1:
+  • Update UI for some screens.
+
 - Version 0.4.0:
   • Rework UI.
   • Support dark theme.
